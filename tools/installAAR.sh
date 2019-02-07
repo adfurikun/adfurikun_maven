a96701a22e276f0218f3deeec8d550510d5fbebd
orei.son <orei.son@gree.net>
2019-02-07 15:45:15 +0900

    update tools

--- tools/installAAR.sh
+++ tools/installAAR.sh
@@ -2,15 +2,15 @@
 
 GROUP=jp.adfully
 ARTIFACT=adfurikunsdk
-VERSION=3.1.0
+VERSION=3.2.0-SNAPSHOT
 PACKAGING=aar
 
-mvn clean dependency:copy-dependencies -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dtype=$PACKAGING -U
+mvn clean dependency:copy -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dtype=$PACKAGING -U
 
 mvn install:install-file -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION \
--Dfile=target/dependency/$ARTIFACT-$VERSION.$PACKAGING \
+-Dfile=target/$ARTIFACT-$VERSION.$PACKAGING \
 -Dpackaging=$PACKAGING \
--DgeneratePom=true \
+-DpomFile=target/$ARTIFACT-$VERSION.pom \
 -DlocalRepositoryPath=../ \
 -DcreateChecksum=true
 
