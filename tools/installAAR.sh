2930c8b62f22cc748f10f5b045d20d84aa12b6c3
orei.son <orei.son@gree.net>
2019-02-13 17:58:06 +0900

    update to 3.1.2

--- tools/installAAR.sh
+++ tools/installAAR.sh
@@ -2,7 +2,7 @@
 
 GROUP=jp.adfully
 ARTIFACT=adfurikunsdk
-VERSION=3.1.1
+VERSION=3.1.2
 PACKAGING=aar
 
 mvn clean dependency:copy -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dtype=$PACKAGING -U
